f8241bb6a6d29703f7874acf9e7586a6fe872320
AcroTaken <sven.nguyen@yahoo.com>
2026-03-25 09:56:28 -0500

    Wizard Crud Done! Close #1

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/converter/WizardDtoToWizardConverter.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/converter/WizardDtoToWizardConverter.java
@@ -6,13 +6,14 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WizardToWizardDtoConverter implements Converter<Wizard, WizardDto> {
+public class WizardDtoToWizardConverter implements Converter<WizardDto, Wizard> {
 
     @Override
-    public WizardDto convert(Wizard source) {
-        return new WizardDto(source.getId(),
-                source.getName(),
-                source.getNumberOfArtifacts());
+    public Wizard convert(WizardDto source) {
+        Wizard wizard = new Wizard();
+        wizard.setId(source.id());
+        wizard.setName(source.name());
+        return wizard;
     }
 
 }
