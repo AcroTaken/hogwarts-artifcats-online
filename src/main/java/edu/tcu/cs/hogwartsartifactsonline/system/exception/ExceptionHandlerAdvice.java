ec22e592aba770c67dcbce9eefa304b76ed6c9b4
AcroTaken <sven.nguyen@yahoo.com>
2026-03-25 10:31:44 -0500

    Wizard CRUD done! Close #2

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
@@ -1,6 +1,5 @@
 package edu.tcu.cs.hogwartsartifactsonline.system.exception;
 
-import edu.tcu.cs.hogwartsartifactsonline.artifact.ArtifactNotFoundException;
 import edu.tcu.cs.hogwartsartifactsonline.system.Result;
 import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
 import org.springframework.http.HttpStatus;
@@ -18,17 +17,17 @@ import java.util.Map;
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
-    @ExceptionHandler(ArtifactNotFoundException.class)
+    @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    Result handleArtifactNotFoundException(ArtifactNotFoundException ex) {
+    Result handleObjectNotFoundException(ObjectNotFoundException ex){
         return new Result(false, StatusCode.NOT_FOUND, ex.getMessage());
     }
 
     /**
      * This handles invalid inputs.
      *
-     * @param ex the validation exception
-     * @return a result containing validation error details
+     * @param ex
+     * @return
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -40,7 +39,7 @@ public class ExceptionHandlerAdvice {
             String val = error.getDefaultMessage();
             map.put(key, val);
         });
-        return new Result(false, StatusCode.INVALID_ARGUMENT,
-                "Provided arguments are invalid, see data for details.", map);
+        return new Result(false, StatusCode.INVALID_ARGUMENT, "Provided arguments are invalid, see data for details.", map);
     }
+
 }
