199e5e3f05bee43979131c78545974ae81939197
AcroTaken <sven.nguyen@yahoo.com>
2026-03-25 09:24:38 -0500

    Artifact CRUD done! Close #1

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsArtifcatsOnlineApplication.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsArtifcatsOnlineApplication.java
@@ -1,13 +1,20 @@
-package edu.tcu.cs.hogwartsartifcatsonline;
+package edu.tcu.cs.hogwartsartifactsonline;
 
+import edu.tcu.cs.hogwartsartifactsonline.artifact.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HogwartsArtifcatsOnlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HogwartsArtifcatsOnlineApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
     }
 
 }
